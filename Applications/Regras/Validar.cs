63b546c635b02872be99c4365b82980b1448fe00
Queiroz754 <allan754@outlook.com>
2026-03-25 10:57:29 -0300

    :sparkles: feat: CRU(Create,Read,Update) área

---
+++ Applications/Regras/Validar.cs
@@ -0,0 +1,15 @@
+﻿using GerenciamentoPatrimonio.Execeptions;
+
+namespace GerenciamentoPatrimonio.Applications.Regras
+{
+    public class Validar
+    {
+        public static void ValidarNome(string nome)
+        {
+            if(string.IsNullOrWhiteSpace(nome))
+            {
+                throw new DomainException("Nome é obrigatorio.");
+            }
+        }
+    }
+}
--- Repository/AreaRepository.cs
+++ Repository/AreaRepository.cs
@@ -17,5 +17,39 @@ namespace GerenciamentoPatrimonio.Repository
         {
             return _context.Area.OrderBy(area => area.NomeArea).ToList();
         }
+
+        public Area BuscarPorId(Guid areaId)
+        {
+            return _context.Area.Find(areaId);
+        }
+
+        public Area BuscarPorNome(string nomeArea)
+        {
+            return _context.Area.FirstOrDefault(area => area.NomeArea.ToLower() == nomeArea.ToLower());
+        }
+
+        public void Adicionar(Area area)
+        {
+            _context.Area.Add(area);
+            _context.SaveChanges();
+        }
+
+        public void Atualizar(Area area)
+        {
+            if(area == null)
+            {
+                return;
+            }
+
+            Area areaBanco = _context.Area.Find(area.AreaID);
+
+            if (area == null)
+            {
+                return;
+            }
+
+            areaBanco.NomeArea = area.NomeArea;
+            _context.SaveChanges();
+        }
     }
 }
