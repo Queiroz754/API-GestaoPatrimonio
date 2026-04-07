ede10a0482a6b5b207c2fd55a708b72f6382cfda
Queiroz754 <allan754@outlook.com>
2026-04-07 11:38:58 -0300

    :construction: Começando a camada de Usuario

---
+++ Repository/UsuarioRepository.cs
@@ -0,0 +1,38 @@
+﻿using GerenciamentoPatrimonio.Contexts;
+using GerenciamentoPatrimonio.Domains;
+using GerenciamentoPatrimonio.Interfaces;
+
+namespace GerenciamentoPatrimonio.Repository
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly GestaoPatrimoniosContext _context;
+
+        public UsuarioRepository(GestaoPatrimoniosContext context)
+        {
+            _context = context;
+        }
+
+        public List<Usuario> Listar()
+        {
+            return _context.Usuario.OrderBy(usuario =>  usuario.NOME).ToList();
+        }
+
+        public Usuario BuscarPorId(Guid usuarioId)
+        {
+            return _context.Usuario.Find(usuarioId);
+        }
+
+        public Usuario BuscarDuplicado(string nif, string cpf, string email, Guid? usuarioId = null)
+        {
+            var consulta = _context.Usuario.AsQueryable();
+
+            if (usuarioId.HasValue)
+            {
+                consulta = consulta.Where(usuario => usuario.UsuarioID != usuarioId.Value);
+            }
+
+            return consulta.FirstOrDefault(usuario =>)
+        }
+    }
+}
